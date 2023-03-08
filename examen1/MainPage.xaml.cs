1c9a487e96d7b2c8ce81dc2f99106c290a81b23e
RAQUEL <RAQUEL@LAPTOP-TMEAL0V3>
2023-03-07 22:55:35 -0600

    Add project files.

---
+++ examen1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace examen1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //llenamos los campos de la lista
+            var listapais = new List<string>();
+            listapais.Add("Belice(501)");
+            listapais.Add("Guatemala(502)");
+            listapais.Add("El Salvador(503)");
+            listapais.Add("Honduras(504)");
+            listapais.Add("Nicaragua(505)");
+            listapais.Add("Costa Rica(506)");
+            listapais.Add("Panamá(507)");
+
+            piker.ItemsSource = listapais;
+        }
+    }
+}
---
+++ examen1/Models/contacto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace examen1.Models
+{
+    public class contacto
+    {
+     int id {  get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set;}
+        public string edad { get; set; }
+        public string pais { get; set; }
+        public string nota { get; set; }
+        public string longitud { get; set;}
+        public string latitud { get; set;}
+
+    }
+
+}
